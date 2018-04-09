2b03dcfd847ee2f62f7909e663321375c9404b68
victor! <vicdacunha@msn.com>
2018-04-09 15:42:26 +0100

    Active Frame update
    Mexi no css, adicionei pesquisa por nome ou por tipo.

--- FrontEnd/my-app/src/ActiveFrame.js
+++ FrontEnd/my-app/src/ActiveFrame.js
@@ -1,37 +1,106 @@
 import React, { Component } from 'react';
+import ActiveList from './ActiveList.js';
+import ActiveSearch from './ActiveSearch.js';
+import './activeframe.css';
 
-
-class Actives_Frame
- extends Component{
+class ActiveFrame extends Component{
 
 	constructor(props){
 		super(props);
 		this.state = {
-			data: props.data,
-			filter_text: props.filter_text,
-			filter_type: props.filter_type,
-			variation_time: props.variation_time
+			data:[ 
+			{
+				id:'AMZ',
+				name: 'AMZ',
+				img: 'default.png',
+				type:'Action action',
+				sell_price: 10.5,
+				variation: [ -0.2,0.5,0.1],
+
+			},
+			{
+				id:'APPZ',
+				name: 'APPZ',
+				img: 'default.png',
+				type:'Action action',
+				sell_price: 99.5,
+				variation: [ -0.5,0.1,0.0],
+
+			},{
+				id:'ETHER',
+				name: 'ETHEREUM',
+				img: 'default.png',
+				type:'Action action',
+				sell_price: 1000.33,
+				variation: [ -0.5,-0.1,-1.0],
+
+			}],
+			updated: false,
+			search_text:'',
+			search_type:'',
+			selected_variation: '0',
+			update_interval: 500 //ms
+
 		}
 	}
 
-	filter_data_byName(data){
-		return data.filter( data_row => data_row.name.substring(this.state.filter_text) !== -1);
-	}
-	filter_data_byType(data){
-		return data.filter( data_row => data_row.type.substring(this.state.filter_text) !== -1);
+	handleSelectedChange(){
+		const docelem = document.getElementById("variacao_select");
+		this.setState({
+			selected_variation: docelem.options[docelem.selectedIndex].value
+		});
 	}
 
+	handleSearchSubmit(searchtext,type){
+		this.setState({
+			search_text:searchtext,
+			search_type:type
+		})
+	}
 
 	render(){
-		let toShowData = [];
-		if(this.state.filter_type === 'Name')
-			toShowData = this.filter_data_byName(this.state.data);
-		else if( this.state.filter_type === 'Type')
-			toShowData = this.filter_data_byType(this.state.data);
+		return (
+			<div>
+			<div className='searchbar'>
+				<ActiveSearch
+					placeholder="Pesquisa..."
+					onSubmit={(text,type) => this.handleSearchSubmit(text,type)} 
+				/>
+			</div>
+			<div className='table_div'>
+				<table className='table'>
+ 				 <thead>
+     				 <tr>
+       					 <th className='name-header'> Nome do ativo </th>
+       					 <th className='variation-header'> <span>Variacao  </span>
+       					 	<span>
+       					 		<select id='variacao_select' onChange={ () => this.handleSelectedChange()} >
+  									<option value="0" defaultValue>1Horas</option>
+  									<option value="1">4Horas</option>
+  									<option value="2">1Dia</option>
+								</select>
+							</span>
+						</th>
+       					 <th className='sellprice-header'> Preço de venda </th>
+       					 <th className='buyprice-header'> Preço de compra </th>
+   				 	  </tr>
+ 				 </thead>
+				</table>
+			</div>
+			<div className="activeList" >
+				<ActiveList 
+						data= {this.state.data}
+						filter_text= {this.state.search_text}
+						filter_type= {this.state.search_type}
+						variation_time={ this.state.selected_variation}
+				 />
+			</div>
+			</div>)
+
 	}
 
 
 }
 
 
-export default Actives_Frames;
+export default ActiveFrame;
--- FrontEnd/my-app/src/index.js
+++ FrontEnd/my-app/src/index.js
@@ -2,6 +2,8 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoApp from './TodoApp.js';
+//import TodoApp from './TodoApp.js';
+import App from './App.js';
 
-ReactDOM.render(<TodoApp title='react todos' />, document.getElementById('root'));
+
+ReactDOM.render(<App title='react todos' />, document.getElementById('root'));
