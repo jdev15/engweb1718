0f65a16ddc73fd2117543945b0892b3f1798a7d9
John Smith <joao_matela96@hotmail.com>
2018-05-24 21:22:38 +0100

    Limpado repositório

--- FrontEnd/index.js
+++
@@ -1,9 +0,0 @@
-import './index.css';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-//import TodoApp from './TodoApp.js';
-import App from './App.js';
-
-
-ReactDOM.render(<App title='react todos' />, document.getElementById('root'));
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TopMenu from './TopMenu.js';
-import './topmenu.css';
-
-ReactDOM.render(<TopMenu title='TopMenu' />, document.getElementById('root'));
