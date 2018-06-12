5ea2729031d33414a7ffc906028e108e6ebad5b9
victor! <vicdacunha@msn.com>
2018-06-12 22:03:46 +0100

    service fase 2
    Mudificação do metodo de atualização da BD fazendo um batch request para os dados.

--- Backend/services/service.js
+++ Backend/services/service.js
@@ -15,7 +15,7 @@ const symbol = client
 					.then(db => db.collection(collectionName[1]))
 					.catch(error => {console.log(error);});
 
-const timer = Number( process.env.TIMER_TIME || '60'); // seconds
+const timer = Number( process.env.TIMER_TIME || '60') * 1000; // milliseconds
 
 function getSymbols(){
 		return symbol.then( collection => collection.find({}).toArray())
@@ -83,12 +83,12 @@ function goToAPI(date,indice,symbol){
         			url: `https://api.iextrading.com/1.0/stock/${symbol.symbol}/chart/date/${day}`, //${present_day}
        	 			json: true,
     	})
-    	.then(response =>{ return response[indice].marketClose;});
+    	.then(response =>{ /*console.log(response);*/ return response[indice].marketClose;});
 }
 
 // Problema como se faz?? Verifico que existe na bd ou faço pedidos??
 // Se não esta na BD, tenho que fazer pedido.
-function variancia1d(last,diatemporal,indice,symbol){
+function variancia1d(diatemporal,indice,symbol){
 	const diaanterior = diaAnterior(diatemporal);
 	//console.log(diaanterior);
 	const dia = { date:`${diaanterior.getUTCDate()}-${diaanterior.getUTCMonth()+1}-${diaanterior.getUTCFullYear()}`,
@@ -110,10 +110,11 @@ function getLast(data){
 }
 
 function calculaIndice(date){
-	// 14:30 até 20h PT
-
+	// 14:30 até 21h UTC
+	// 9:30 até 16h
 	return (date.getUTCHours() * 60 + date.getUTCMinutes()) - 870;
 }
+
 function processElement(data,date,symbol){
 	// No maximo são 390 registos. Transformar a minha hora em um indice.
 	// 
@@ -129,7 +130,7 @@ function processElement(data,date,symbol){
 	// correspondendo ao valor de fecho presente em 389.
 		// a variancia de 1d pode ser feita usando a bd (caso existe valores para o dia anterior)
 		// ou fazendo mais um pedido a API...
-		const variacao1d = variancia1d(last, date, indice,symbol);
+		const variacao1d = variancia1d( date, indice,symbol);
 		const value = variacao1d.then(valor =>  new Promise((a,b) => a({
 							nome: symbol.name,
 							simbolo: symbol.symbol,
@@ -146,18 +147,98 @@ function processElement(data,date,symbol){
 					 				  },
 					 		timestamp: date.valueOf()
 							})  ));
-	 	return value
-
-
+	 	return value;
 }
 
 
+function variancia1h2(data,indice,value){
+	if( indice < 60){
+		return  value - data[0].marketOpen;
+	}else return value - data[indice-60].marketClose;
+}
 
-function recoverDataFromIEXDB(symbols){
+function variancia4h2(data,indice,value){
+	if( indice < 240){
+		return value - data[0].marketOpen;
+	}else return value - data[indice-240].marketClose;
+}
+
+function processElement2(dados,adata){
+	// dados tem: a quote (com o simbolo dentro)
+	// 			  a chart (os valores do dia)
+	let value;
+	const indice = calculaIndice(adata);
+	console.log(indice);
+	if( indice < 0 || indice > 390) // fora das horas
+		return; // Não se calcula cenas fora das horas para não dar problemas na BD
+	if( indice === 0 || indice === 390){
+			// nos limites usa-se o quote só !!
+			console.log("hello");
+			let variacao1d;
+			if( indice === 390) variacao1d = variancia1d( adata, indice-1,dados.quote);
+			else variacao1d = variancia1d( adata, indice,dados.quote);
+
+			value = variacao1d.then(valor =>  new Promise((a,b) => a({
+							nome: dados.quote.companyName,
+							simbolo: dados.quote.symbol,
+							preco_de_venda: dados.quote.latestPrice,
+							preco_de_compra: dados.quote.latestPrice * 1.03,
+							variacaos: {
+											d1: dados.quote.latestPrice - valor,
+											h4: variancia4h2(dados.chart, indice,dados.quote.latestPrice),
+											h1: variancia1h2(dados.chart, indice,dados.quote.latestPrice)
+							},
+							datetime: {	// dia-mes-ano e hh-mm os segundos (no nosso caso) não são tão importantes
+											date:`${date.getUTCDate()}-${date.getUTCMonth()+1}-${date.getUTCFullYear()}`,
+					 						time: `${date.getUTCHours()}:${date.getUTCMinutes()}`
+					 				  },
+					 		timestamp: date.valueOf()
+							})  ));
+
+	}else { // de resto pode se usar o quote para o preço de venda e compra.
+		const variacao1d = variancia1d(adata, indice,dados.quote);
+		value = variacao1d.then(valor =>  new Promise((a,b) => a({
+							nome: dados.quote.companyName,
+							simbolo: dados.quote.symbol,
+							preco_de_venda: dados.quote.latestPrice,
+							preco_de_compra: dados.quote.latestPrice * 1.03,
+							variacaos: {
+											d1: dados.quote.close - valor,
+											h4: variancia4h2(dados.chart, indice,dados.quote.latestPrice),
+											h1: variancia1h2(dados.chart, indice,dados.quote.latestPrice)
+							},
+							datetime: {	// dia-mes-ano e hh-mm os segundos (no nosso caso) não são tão importantes
+											date:`${date.getUTCDate()}-${date.getUTCMonth()+1}-${date.getUTCFullYear()}`,
+					 						time: `${date.getUTCHours()}:${date.getUTCMinutes()}`
+					 				  },
+					 		timestamp: date.valueOf()
+							})  ));
+	}
+	 	return value;
+}
+
+
+//Batch request getting quote + chart
+function recoverDataFromIEXDB2(symbols,date){
+	let simbols = [];
+	let lista = "";
+	symbols.forEach(s =>{ simbols.push(s.symbol); lista= lista + s.symbol + ','});
+	console.log(lista);
+	// Agora que eu tenho ambos posso processar.
+	return  request({
+	        			url: `https://api.iextrading.com/1.0/stock/market/batch?symbols=${lista}&types=quote,chart&range=1d`, 
+	        			json: true /* Esta request da-me a quote e o chart no dia, para cada simbolo*/ 
+	        		}).then(response => simbols.map( simbolo => processElement2(response[simbolo],date) ))
+	        		  .catch(error => console.log(error))
+    				;
+}
+
+// 1 request de cada vez para cada um deles, chart
+function recoverDataFromIEXDB(symbols,date){
 	// recupero os ultimos dados para a minha lista de simbolos
 	// calculo as varianças para 1D, 4H e 1H para os meus simbolos.
 	//  Guardo as varianças na BD ?
-	const date = new Date();
+	//const date = new Date();
 	//console.log(symbols);
 	const present_day = createAPIDate(date);
 	// testar com https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,fb&types=quote,chart&range=1d&chartLast=2
@@ -167,23 +248,34 @@ function recoverDataFromIEXDB(symbols){
 	return Promise.all(symbols.map( symbol => 	request({
 	        								url: `https://api.iextrading.com/1.0/stock/${symbol.symbol}/chart/date/${present_day}`, 
 	        						    	json: true 
-	        						    }).then(response => processElement(response,date,symbol) )
+	        						    }).then(response => processElement(response,date,symbol) ).catch(error => console.log(error))
+								
     				 ));
-
 }
 
 
 function updateDB(){
 	if( !isMarketOpen()) return;
+	const date = new Date();
 	symbol.then( db => db.find({}).toArray())
-		  .then(symbols => recoverDataFromIEXDB(symbols))
+		  .then(symbols => recoverDataFromIEXDB(symbols,date))
+		  .then( novosdados => ativos.then(db => db.insertMany(novosdados)))
+		  .then( answer => {console.log("Inserted"); console.log(answer)})
+		  .catch(error => console.log(error));
+}
+
+function updateDB2(){
+	if( !isMarketOpen()) return;
+	const date = new Date();
+	symbol.then( db => db.find({}).toArray())
+		  .then(symbols => recoverDataFromIEXDB2(symbols,date))
+		  .then( d => Promise.all(d))
 		  .then( novosdados => ativos.then(db => db.insertMany(novosdados)))
 		  .then( answer => {console.log("Inserted"); console.log(answer)})
 		  .catch(error => console.log(error));
 }
 
 
-
-setInterval(() =>console.log(updateDB()) , timer * 1000)
+setInterval(() =>console.log(updateDB2()) , timer)
 //console.log(isMarketOpen());
 updateDB();
